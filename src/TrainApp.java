f170a82650f0a60e1448f047c111dfb7f3571bb9
vin2007 <vineshreddy2006@gmail.com>
2026-03-30 15:21:32 +0530

    UC3 f

---
+++ src/TrainApp.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        Set<String> bogieIDs = new HashSet<>();
+
+        bogieIDs.add("BG101");
+        bogieIDs.add("BG102");
+        bogieIDs.add("BG103");
+        bogieIDs.add("BG101");
+        bogieIDs.add("BG102");
+
+        System.out.println("Unique Bogie IDs in Train Consist:");
+        System.out.println(bogieIDs);
+
+        System.out.println("Program continues...");
+    }
+}
